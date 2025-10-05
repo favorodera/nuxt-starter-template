481bdc2878a7e172a0839e25f2d471bb69ae9755
Favour  Emeka <favorodera@gmail.com>
2025-10-05 19:50:18 +0100

    refactor: remove skip link from app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,4 @@
 <template>
-  <a
-    href="#main"
-    class="sr-only focus:not-sr-only focus:fixed focus:top-2 focus:left-2 focus:z-50 focus:rounded focus:bg-default focus:px-3 focus:py-2 focus:text-default"
-  >
-    Skip to main content
-  </a>
   <UApp
     id="main"
     tabindex="-1"
