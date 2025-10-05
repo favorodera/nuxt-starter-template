56633a5238cc68ba9b10480077729ddaf0abb53f
Favour  Emeka <favorodera@gmail.com>
2025-10-05 02:03:39 +0100

    refactor: clean up nuxt.config.ts and update package.json scripts
    - Removed unused modules and configurations from nuxt.config.ts
    - Added linting and staging scripts to package.json
    - Updated dependencies for better compatibility

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,21 +7,12 @@ export default defineNuxtConfig({
     '@nuxt/ui',
     '@vueuse/nuxt',
     '@nuxt/eslint',
-    '@vaxee/nuxt',
-    '@nuxtjs/seo',
   ],
-  imports: {
-    dirs: ['./app/stores'],
-  },
   devtools: { enabled: true },
   app: {
     rootTag: 'main',
     head: {
       htmlAttrs: { lang: 'en', dir: 'ltr' },
-      link: [
-        { rel: 'icon', href: '/favicon-dark.ico', media: '(prefers-color-scheme:dark)' },
-        { rel: 'icon', href: '/favicon-light.ico', media: '(prefers-color-scheme:light)' },
-      ],
     },
     rootAttrs: {
       id: 'app',
@@ -30,13 +21,6 @@ export default defineNuxtConfig({
     layoutTransition: { name: 'fade-out-in', mode: 'out-in' },
   },
   css: ['~/assets/styles/index.css'],
-  site: {
-    url: 'https://url.com',
-    name: 'Name',
-  },
-  experimental: {
-    viewTransition: true,
-  },
   compatibilityDate: '2025-07-15',
   vite: {
     $server: {
@@ -62,26 +46,5 @@ export default defineNuxtConfig({
     customCollections: [
       { prefix: 'custom', dir: './app/assets/icons' },
     ],
-  },
-  seo: {
-    redirectToCanonicalSiteUrl: true,
-    meta: {
-      titleTemplate: '%separator %s',
-      description: 'Description',
-      ogDescription: 'OG description',
-      twitterDescription: 'Twitter description',
-      twitterCard: 'summary_large_image',
-      twitterSite: '@favorodera',
-      twitterCreator: '@favorodera',
-      colorScheme: 'dark light',
-      author: 'Favour Emeka',
-      themeColor: [
-        { content: '#000000', media: '(prefers-color-scheme: dark)' },
-        { content: '#ffffff', media: '(prefers-color-scheme: light)' },
-      ],
-      appleMobileWebAppStatusBarStyle: 'black-translucent',
-      viewport: 'width=device-width, initial-scale=1',
-      charset: 'utf-8',
-    },
   },
 })
